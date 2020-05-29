0fa2acb0e9d4ce56258820d49cd3102acb8e5713
c1tru5x <gino.chris28@gmail.com>
2020-05-29 13:04:19 +0200

    checks and corrects user input

--- corona-checkin-ou/MainWindow.xaml.cs
+++ corona-checkin-ou/MainWindow.xaml.cs
@@ -1,19 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-
+/// <todo>
+/// BUGFIXES:
+/// 
+/// if checkout is used when already 0
+/// no checkin will work
+/// ------------------------------------
+/// 
+/// </todo>
 
 namespace corona_checkin_ou
 {
@@ -22,61 +18,95 @@ namespace corona_checkin_ou
     /// </summary>
     public partial class MainWindow : Window
     {
-        public int maxPersons = 0;
-        public int scale = 2;
+        private int maxPersons = 0;
+        private const int scale = 2;
+        //private const int upperlimit = 10000;
 
         public MainWindow()
         {
             InitializeComponent();
+            pb.Value = 0;
+            PersonCounter.Text = "0";
+            pInside.Content = "Persons Inside: 0";
         }
 
         private void ProgressBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             if(PersonCounter.Text != "")
             {
-                // to get full bar / 2
-                pb.Maximum = Convert.ToDouble(PersonCounter.Text) / 2;
-            }
-            else
-            {
-                pb.Value = 0;
+                // to get full bar / scale and math.floor
+                pb.Maximum = Math.Floor(Convert.ToDouble(PersonCounter.Text) / scale);
             }
         }
 
         //Check IN
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(pb.Value != maxPersons)
+            if(pb.Value != maxPersons + 1 && IsNum())
             { 
                 pb.Value++;
                 pInside.Content = "Persons Inside: " + Convert.ToString(pb.Value);
             }
-            if (pb.Value > maxPersons / 2)
+            else
             {
-                //color red
-
+                PersonCounter.Text = "0";
+                pb.Value = 0;
             }
         }
 
         //Check OUT
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            if(pb.Value != 0 && pb.Value > 0)
+            if (pb.Value > 0 && IsNum())
             { 
                 pb.Value--;
                 pInside.Content = "Persons Inside: " + Convert.ToString(pb.Value);
             }
-            if(pb.Value <= maxPersons/2)
+            else
             {
-                //color green
-                
+                PersonCounter.Text = "0";
+                pb.Value = 0;
             }
         }
 
         //Get maximum amount of persons (half of total persons)
         private void PersonCounter_TextChanged(object sender, TextChangedEventArgs e)
         {
-            maxPersons =  Convert.ToInt32(PersonCounter.Text) / 2;
+            //get pb.maximum
+            if(IsNum())
+            {
+                pb.Maximum = Math.Floor(Convert.ToDouble(PersonCounter.Text) / scale);
+            }
+            
+            //if empty set to 0
+            if (PersonCounter.Text == "")
+            {
+                PersonCounter.Text = "0";
+                pb.Value = 0;
+            }
+            if(!IsNum())
+            {
+                MessageBox.Show("Please enter only a number!", "Input Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+                //remove last input
+                PersonCounter.Text = PersonCounter.Text.Remove(PersonCounter.Text.Length - 1);
+            }
+            else
+            {
+                maxPersons =  Convert.ToInt32(PersonCounter.Text) / scale;
+            }
+        }
+
+        //check if textinput is a number
+        private bool IsNum()
+        {
+            if (System.Text.RegularExpressions.Regex.IsMatch(PersonCounter.Text, "[^0-9]"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
